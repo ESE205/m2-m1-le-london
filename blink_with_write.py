84ad7c3a3a0a548442d6943cdead730130353fb3
selena <london.s@wustl.edu>
2024-01-26 12:46:50 -0600

    01/26done

--- blink_with_write.py
+++ blink_with_write.py
@@ -1,3 +1,5 @@
+import time
+import sys
 import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setwarnings(False)
@@ -5,15 +7,23 @@ GPIO.setmode(GPIO.BOARD)
 
 pin1=11
 pin2=12
+iter=5
 
 GPIO.setup(pin1, GPIO.IN)
 GPIO.setup(pin2, GPIO.OUT)
 GPIO.output(pin2, GPIO.LOW)
 
-while True:
-  input = GPIO.input(pin1)
-  if input==True:
-    GPIO.output(pin2, GPIO.HIGH)
-  else:
-    GPIO.output(pin2, GPIO.LOW)
+with open('data.txt', 'w') as data:
+  while iter>0:
+    iter -= 1
+    input = GPIO.input(pin1)
+    if input==True:
+      GPIO.output(pin2, GPIO.HIGH)
+      data.write(f'{time.time():1.0f} {input}\n')
+      sleep(1)
+      GPIO.output(pin2, GPIO.LOW)
+      sleep(1)
+    else:
+      GPIO.output(pin2, GPIO.LOW)
 GPIO.cleanup()
+
